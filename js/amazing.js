e2ecc6309e4ce645e3cebba6c896e56e7068e21c
Wayne Larson Jr <wlarson30@gmail.com>
2018-10-20 05:09:09 -0300

    updated the upload page template javascript

--- js/amazing.js
+++ js/amazing.js
@@ -1,4 +1,9 @@
-function selectionChanged()
+function selectionChangedUploadForm()
 {
-  console.log("selection changed");
+  document.getElementById('photo_upload_form').style.display = 'none';
+  document.getElementById('photoalbum_upload_form').style.display = 'none';
+  document.getElementById('video_upload_form').style.display = 'none';
+  document.getElementById('mobile_upload_form').style.display = 'none';
+  console.log(document.getElementById('upload_post_type').value);
+  document.getElementById(document.getElementById('upload_post_type').value).style.display = 'block';
 }
--- page-templates/upload_form.php
+++ page-templates/upload_form.php
@@ -18,24 +18,46 @@ Template Name: Upload
     <input type="text" id="upload_title">
     <br>
     <lable class="AWLable" for="upload_post_type">Post Type</lable>
-    <select id="upload_post_type" onchange="selectionChanged()">
-       <option value="photo">Photo</option>
-       <option value="photoalbum">Photo Album</option>
-       <option value="video">Video</option>
+    <select id="upload_post_type" onchange="selectionChangedUploadForm()">
+       <option value="photo_upload_form">Photo</option>
+       <option value="photoalbum_upload_form">Photo Album</option>
+       <option value="video_upload_form">Video</option>
+       <option value="mobile_upload_form">Mobile</option>
     </select>
+
+    <div  id="photo_upload_form">
+      <lable class="AWLable" for="aw_photo_tags">Tags:</lable>
+      <input type="text" id="aw_photo_tags" name="tags"><br>
+      <lable class="AWLable" for="aw_photo_people">People:</lable>
+      <input type="text" id="aw_photo_people" name="people"><br>
+      <label class="AWLable" for="aw_photo_upload">Upload</label>
+      <input type="file" name="aw_photo_upload" id="aw_photo_upload">
+    </div>
+
+    <div id="mobile_upload_form" class="aw_post_upload_form">
+
+    </div>
+
+    <div id="photoalbum_upload_form" class="aw_post_upload_form">
+
+    </div>
+
+    <div id="video_upload_form" class="aw_post_upload_form">
+
+    </div>
     <br>
-    <label class="AWLable" for="aw_photo_upload">Upload</label>
-    <input type="file" name="aw_photo_upload" id="aw_photo_upload">
+
     <br>
     <label class="AWLable" for="aw_upload_submit_button"></label>
-    <button id="aw_upload_submit_button" class="Button ButtonColor" type="submit">Submit</button>
+    <button id="aw_upload_submit_button" class="Button ButtonColor" style="padding:5px" type="submit">Submit</button>
     <?php //wp_insert_post; ?>
   </form>
+
+  <script>
+
+  </script>
+
+  <?php get_footer(); ?>
 </main>
 
 <!-- Create page footer -->
-<?php get_footer(); ?>
-
-<script type="text/script">
-
-</script>
