91e21a20315c7e3152bf4b235518085040fe67a2
Wayne Larson Jr <wlarson30@gmail.com>
2018-11-16 08:39:32 -0400

    added the aw_single_post_image css class to the main image displayed by thne single.php template. This class centers the image.

--- single.php
+++ single.php
@@ -47,7 +47,7 @@
 						{
 							$url = wp_get_attachment_url($value);
 
-							echo '<a href="'.$url.'"><img src="'.$url.'"></a>';
+							echo '<a href="'.$url.'"><img class="aw_single_post_image" src="'.$url.'"></a>';
 						}
 							?>
 
