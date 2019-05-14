0d06adc6302ce2764904407d904f445503e2080c
Wayne Larson Jr <wlarson30@gmail.com>
2019-05-14 19:28:20 -0300

    Changed single-photoalbum class attributes

--- single-photoalbum.php
+++ single-photoalbum.php
@@ -45,7 +45,7 @@
 								{
 									$link = wp_get_attachment_url($value);
 									$image = wp_get_attachment_image_url($value);
-									echo '<div class="photo_album_item" style="margin:5px; display:inline-block;">';
+									echo '<div class="thumbnail" style="margin:5px; display:inline-block;">';
 									echo '<a href="'.$link.'"><img src="'.$image.'"></a>';
 									echo '</div>';
 								}
