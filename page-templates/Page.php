405c6469845e8ede27bc20a8bb933e129f4273b0
Wayne Larson Jr <wlarson30@gmail.com>
2019-05-24 01:36:13 -0300

    Updated Page.php with new layout code

--- page-templates/Page.php
+++ page-templates/Page.php
@@ -1,23 +1,33 @@
 <?php
-/*
-Template Name: Page Template
-*/
+	/*
+	Template Name: Page Template
+	*/
 ?>
 
-
-<!-- Start the loop -->
 <?php get_header(); ?>
-<?php get_sidebar( 'primary' ); ?>
+<!-- print the name of the page when in debug mode -->
+<?php aw_print_name('Contact.php Page Template'); ?>
+<?php get_template_part('/Templates/Parts/post header'); ?>
+<div id="row_2">
+  <div id="row_2_column_1" class="">
+    <main class="clearfix">
+      <?php if ( have_posts() ) : ?>
+        <?php while ( have_posts() ) : ?>
+          <?php the_post(); ?>
+         <?php get_template_part('/Templates/Parts/post'); ?>
+        <?php endwhile; ?>
+      <?php endif; ?>
+    </main>
 
-<?php
-	global $post;
-	echo $post->post_content;
-?>
-<!-- Create post navigation menu -->
-<div style="clear:left"></div>
-<nav class="post_navigation_menu">
-   <?php aw_numeric_posts_nav(); ?>
-</nav>
-</br>
-</br>
-<?php get_footer(); ?>
+		<!-- Add comments to template -->
+		 <?php if ( comments_open() || get_comments_number() ) : ?>
+			 <?php comments_template(); ?>
+		 <?php endif; ?>
+		 
+    <!-- Create page footer -->
+    <?php get_footer(); ?>
+  </div>
+  <div id="row_2_column_2" class="">
+    <?php get_sidebar( 'primary' ); ?>
+  </div>
+</div>
