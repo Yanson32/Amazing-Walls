931d3b50d4bfad3aac7d88302f6d03894e2637cc
Wayne Larson Jr <wlarson30@gmail.com>
2019-05-24 01:27:59 -0300

    Updated tags template

--- page-templates/Tags.php
+++ page-templates/Tags.php
@@ -4,65 +4,67 @@ Template Name: Tag Template
 */
 ?>
 
+<?php $alphabet = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'); ?>
 
 <?php get_header(); ?>
-<?php get_sidebar( 'primary' ); ?>
-
-
 <!-- print the name of the page when in debug mode -->
 <?php aw_print_name('tag.php'); ?>
+<?php get_template_part('/Templates/Parts/post header'); ?>
+<div id="row_2">
+  <div id="row_2_column_1" class="">
+    <main class="clearfix">
+      <!-- Create a bunch of links for each letter of the alphabet -->
+      <div class="clearfix">
+        <ul id="aw_tag_list_alphabetical" >
+          <?php foreach($alphabet as $letter)
+          {
+            echo '<li id="aw_tags_button_'.$letter.'" class="Button ButtonColor" onclick="aw_letter_selected(event, &quot;'.$letter.'&quot;);">'.$letter.'</li>';
+          }
+          ?>
+        </ul>
+      </div>
 
-<?php $alphabet = array('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'); ?>
-<div>
-
-  <main class="clearfix">
-
-    <!-- Create a bunch of links for each letter of the alphabet -->
-    <div class="clearfix">
-      <ul id="aw_tag_list_alphabetical" >
-        <?php foreach($alphabet as $letter)
+      <?php
+        foreach($alphabet as $letter)
         {
-          echo '<li id="aw_tags_button_'.$letter.'" class="Button ButtonColor" onclick="aw_letter_selected(event, &quot;'.$letter.'&quot;);">'.$letter.'</li>';
-        }
-        ?>
-      </ul>
-    </div>
-
-    <?php
-      foreach($alphabet as $letter)
-      {
-        echo '<div id="aw_tags_'.$letter.'" style="display:none;">';
-          echo '<ul class="aw_tag_list">';
-            foreach(get_tags() as $tag)
-            {
-              if(empty($tag->name))
-                return;
-
-
-              if(strtoupper($tag->name[0]) == $letter)
+          echo '<div id="aw_tags_'.$letter.'" style="display:none;">';
+            echo '<ul class="aw_tag_list">';
+              foreach(get_tags() as $tag)
               {
-                $tag_link = get_tag_link( $tag->term_id );
-                echo '<li><a Class="Button ButtonColor" href="'.$tag_link.'">'.$tag->name.'</a></li>';
+                if(empty($tag->name))
+                  return;
+
+
+                if(strtoupper($tag->name[0]) == $letter)
+                {
+                  $tag_link = get_tag_link( $tag->term_id );
+                  echo '<li><a Class="Button ButtonColor" href="'.$tag_link.'">'.$tag->name.'</a></li>';
+                }
+
               }
+            echo '</ul>';
+          echo '</div>';
+        }
+    ?>
+    </main>
 
-            }
-          echo '</ul>';
-        echo '</div>';
-      }
-  ?>
-  </main>
+    <!-- Add comments to template -->
+    <?php if ( comments_open() || get_comments_number() ) : ?>
+      <?php comments_template(); ?>
+    <?php endif; ?>
+
+    <!-- Create post navigation menu -->
+    <nav class="post_navigation_menu post_navigation_menu_color">
+      <?php global $wp_query; aw_numeric_posts_nav($wp_query, "Previous", "Next"); ?>
+    </nav>
+
+    <!-- Create page footer -->
+    <?php get_footer(); ?>
+  </div>
+  <div id="row_2_column_2" class="">
+    <?php get_sidebar( 'primary' ); ?>
+  </div>
 </div>
-
-<!-- Add comments to template -->
- <?php if ( comments_open() || get_comments_number() ) : ?>
-   <?php comments_template(); ?>
- <?php endif; ?>
-
-
-<!-- Create post navigation menu -->
-<nav class="post_navigation_menu post_navigation_menu_color">
-  <?php global $wp_query; aw_numeric_posts_nav($wp_query, "Previous", "Next"); ?>
-</nav>
 
 <script>
 /**********************************************************************************//**
@@ -101,5 +103,3 @@ function aw_letter_selected(event, letter)
   }
 }
 </script>
-<!-- Create page footer -->
-<?php get_footer(); ?>
