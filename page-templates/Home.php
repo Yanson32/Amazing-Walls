76a97bda1676d9593c6e7a4e4b8d1f0f8bc5ebde
Wayne Larson Jr <wlarson30@gmail.com>
2019-05-24 01:47:45 -0300

    Updated home.php with new page layout

--- page-templates/Home.php
+++ page-templates/Home.php
@@ -5,34 +5,29 @@
 ?>
 
 <?php get_header(); ?>
-<?php get_sidebar( 'primary' ); ?>
-
-
 <!-- print the name of the page when in debug mode -->
 <?php aw_print_name('Home.php Page Template'); ?>
+<?php get_template_part('/Templates/Parts/post header'); ?>
+<div id="row_2">
+  <div id="row_2_column_1" class="">
+    <main class="clearfix">
+      <?php if ( have_posts() ) : ?>
+        <?php while ( have_posts() ) : ?>
+          <?php the_post(); ?>
+         <?php the_content(); ?>
+        <?php endwhile; ?>
+      <?php endif; ?>
+    </main>
 
+		<!-- Add comments to template -->
+		 <?php if ( comments_open() || get_comments_number() ) : ?>
+			 <?php comments_template(); ?>
+		 <?php endif; ?>
 
-<!-- The Loop -->
-<main>
- <?php if ( have_posts() ) : ?>
-   <?php while ( have_posts() ) : ?>
-     <?php the_post(); ?>
-    <?php get_template_part('/Templates/Parts/post'); ?>
-   <?php endwhile; ?>
- <?php endif; ?>
-</main>
-
-
-<!-- Add comments to template -->
- <?php if ( comments_open() || get_comments_number() ) : ?>
-   <?php comments_template(); ?>
- <?php endif; ?>
-
-
-<!-- Create post navigation menu -->
-<nav class="post_navigation_menu post_navigation_menu_color">
-  <?php global $wp_query; aw_numeric_posts_nav($wp_query, "Previous", "Next"); ?>
-</nav>
-
-<!-- Create page footer -->
-<?php get_footer(); ?>
+    <!-- Create page footer -->
+    <?php get_footer(); ?>
+  </div>
+  <div id="row_2_column_2" class="">
+    <?php get_sidebar( 'primary' ); ?>
+  </div>
+</div>
