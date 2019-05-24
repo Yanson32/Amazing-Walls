7d2e3e3c43253317128d9ab0c2eaf49879da5973
Wayne Larson Jr <wlarson30@gmail.com>
2019-05-24 01:37:39 -0300

    Updated Contact Form.php's main loop

--- page-templates/Contact Form.php
+++ page-templates/Contact Form.php
@@ -14,7 +14,8 @@
       <?php if ( have_posts() ) : ?>
         <?php while ( have_posts() ) : ?>
           <?php the_post(); ?>
-         <?php get_template_part('/Templates/Parts/post'); ?>
+         <?php the_title() ?>
+         <?php the_content() ?>
         <?php endwhile; ?>
       <?php endif; ?>
     </main>
